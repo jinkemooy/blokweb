81a872b6e842ae789a868a75a3d73467f9ee5a4a
jinkemooy <94449426+jinkemooy@users.noreply.github.com>
2021-12-15 01:13:50 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+console.log("lalalal")
+
+var myElement = document.querySelector("footer img");
+myElement.addEventListener("scroll", function(ev){
+    ev.target.scrollTop;//pixels scrolled from element top
+    ev.target.scrollHeight;//pixels of the whole element.
+});
